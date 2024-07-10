a40f4925dba33099231d8e268f108d9acd955857
Charles McCarthy <charlesmccarthy233@gmail.com>
2024-07-10 16:15:47 +0000

    completed mobile view

--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 import hamburger from '../Images/hamburger.png';
+import close from '../Images/close.png';
 
+const Header: React.FC = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-const Header:  React.FC  = () => {
-    return(
-        <div className="header">
-            <span className="hamburger">
-                <img src={hamburger} alt="hamburgur pic" />
-            </span>
-            <span className="logo">
-                <a href="#">room</a>
-            </span>
-            <span className="nav-links">
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  return (
+    <div className="header">
+        <span className="hamburger" onClick={toggleDropdown}>
+          <img src={hamburger} alt="hamburger menu" />
+        </span>
+        <span className="logo">
+            <a href="#">room</a>
+        </span>
+        <span className="nav-links">
+            <a href="#">home</a>
+            <a href="#">shop</a>
+            <a href="#">about</a>
+            <a href="#">contact</a>
+        </span>
+        <div className={`dropdown ${dropdownOpen ? 'show' : ''}`}>
+          <div className="dropdown-content">
+            <div className="close" onClick={toggleDropdown}>
+              <img src={close} alt="close dropdown" />
+            </div>
+            <div>
                 <a href="#">home</a>
                 <a href="#">shop</a>
                 <a href="#">about</a>
-                <a href="#">contact</a>
-            </span>
-            <span className="dropdwon">
-                <a href="#">home</a>
-                <a href="#">shop</a>
-                <a href="#">about</a>
-                <a href="#">contact</a>
-            </span>
+                <a href="#">contact</a> 
+            </div>
+          </div>
         </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Header;
